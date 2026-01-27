db11f1a53410349c31ac0f9a581a886793ac57c1
LeszczynskiKarol <leszczu123@wp.pl>
2026-01-27 19:08:02 +0100

    check

--- aws-lambda/create-checkout/index.mjs
+++ aws-lambda/create-checkout/index.mjs
@@ -58,7 +58,7 @@ export const handler = async (event) => {
 
     // Tworzenie Stripe Checkout Session
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card", "blik", "p24"],
+      payment_method_types: ["card", "blik"],
       line_items: [
         {
           price_data: {
